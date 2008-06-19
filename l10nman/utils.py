5d86eb456ee0ed2386e88fca004444c6ac7ee223
Pedro Algarvio <ufs@ufsoft.org>
2008-06-19 23:59:21 +0100

    AVAILABLE_LOCALES are now under utils to be reused.

--- l10nman/utils.py
+++ l10nman/utils.py
@@ -13,31 +13,16 @@
 # Please view LICENSE for additional licensing information.
 # =============================================================================
 
-from trac.resource import ResourceNotFound
-from trac.util.translation import _
-from trac.versioncontrol import NoSuchChangeset
-from trac.versioncontrol.web_ui.util import get_existing_node
-from trac.web.main import RequestDone
+from babel import localedata
+from babel.core import Locale
 
-from genshi.builder import tag
+AVAILABLE_LOCALES = {}
 
-def ajax_return_repo_paths_list(env, req):
-    repopath = req.args.get('q', '/')
+def _build_locales():
+    locales = map(Locale.parse, localedata.list())
+    for locale in locales:
+        if str(locale) not in AVAILABLE_LOCALES:
+            AVAILABLE_LOCALES[str(locale)] = (
+                str(locale), locale.english_name, locale.display_name)
 
-    repos = env.get_repository(req.authname)
-
-    try:
-        node = get_existing_node(req, repos, repopath, repos.youngest_rev)
-    except NoSuchChangeset, e:
-        raise ResourceNotFound(e.message, _('Invalid Changeset Number'))
-
-    entries = []
-    for entry in node.get_entries():
-        path = entry.path
-        if not path.startswith('/'):
-            path = '/%s' % path
-        if entry.kind == 'dir':
-            path = tag.b(path)
-        entries.append(tag.li(path))
-    req.write(tag.ul(*entries))
-    raise RequestDone
+_build_locales()
